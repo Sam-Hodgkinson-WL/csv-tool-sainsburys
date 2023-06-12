95ef8ee4417e1b3b0d485e6280a6488056251f50
Hodgkinson <sam.hodgkinson@worldline.com>
2023-06-12 14:09:59 +0100

    Removed backend directory as no longer needed and completed all functions to a working prototype minus NUM_NUMCOMM variable, speek to Mike S on this functionality

--- tool/src/components/Data.jsx
+++ tool/src/components/Data.jsx
@@ -1,41 +1,69 @@
-import React, {useState} from 'react'
-import CsvDownloader from 'react-csv-downloader'
-import '../style/data.css'
+import React, { useState } from "react";
+import CsvDownloader from "react-csv-downloader";
+import "../style/data.css";
 
-import {createADM, createCPA, createAGC} from '../functions'
+import { createADM, createCPA, createAGC, createGAX } from "../functions";
 
 function Data() {
-
-    const [rawData, setRawData] = useState(undefined)
+  const [rawData, setRawData] = useState(undefined);
+  const [ADMData, setADMData] = useState(undefined);
+  const [CPAData, setCPAData] = useState(undefined);
+  const [AGCData, setAGCData] = useState(undefined);
+  const [GAXData, setGAXData] = useState(undefined);
 
   return (
     <div>
-        <textarea placeholder='Enter the data from the raw data tab' onChange={(e) => setRawData(e.target.value)}></textarea>
-        <div className='buttons'>
-          <CsvDownloader 
-            text="Download ADM" 
-            datas={() => createADM(rawData)} 
-            filename="ADM"
-            extension='.csv'
-            separator=';'
-          />
-          <CsvDownloader 
-            text="Download CPA" 
-            datas={() => createCPA(rawData)} 
-            filename="CPA"
-            extension='.csv'
-            separator=';'
-          />
-          <CsvDownloader 
-            text="Download AGC" 
-            datas={() => createAGC(rawData)} 
-            filename="AGC"
-            extension='.csv'
-            separator=';'
-          />
-        </div>
+      <button onClick={() => console.log(ADMData)}>Console</button>
+      <textarea
+        placeholder="Enter the data from the raw data tab"
+        onChange={(e) => setRawData(e.target.value)}
+      ></textarea>
+      <div className="generate-buttons">
+        <button onClick={() => setADMData(() => createADM(rawData))}>
+          Generate ADM
+        </button>
+        <button onClick={() => setCPAData(() => createCPA(rawData))}>
+          Generate CPA
+        </button>
+        <button onClick={() => setAGCData(() => createAGC(rawData))}>
+          Generate AGC
+        </button>
+        <button onClick={() => setGAXData(() => createGAX(rawData))}>
+          Generate GAX
+        </button>
+      </div>
+      <div className="buttons">
+        <CsvDownloader
+          text="Download ADM"
+          datas={() => createADM(rawData)}
+          filename="ADM"
+          extension=".csv"
+          separator=";"
+        />
+        <CsvDownloader
+          text="Download CPA"
+          datas={() => createCPA(rawData)}
+          filename="CPA"
+          extension=".csv"
+          separator=";"
+        />
+        <CsvDownloader
+          text="Download AGC"
+          datas={() => createAGC(rawData)}
+          filename="AGC"
+          extension=".csv"
+          separator=";"
+        />
+        <CsvDownloader
+          text="Download GAX"
+          datas={() => createGAX(rawData)}
+          filename="GAX"
+          extension=".csv"
+          separator=";"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Data
+export default Data;
--- tool/src/functions.js
+++ tool/src/functions.js
@@ -1,93 +1,159 @@
+////////////////////////////////////////////////////////////////////
+// Raw Data
+let blankVariableForm = {
+  //****************************** ADM CSV ******************************
+  E_PORTAL_ZONE: undefined,
+  POS_POS_NUMBER: undefined,
+  POS_PCI4: undefined,
+  //****************************** CPA CSV ******************************
+  //E_PORTAL_ZONE DEFINED ABOVE
+  NUM_NUMCOMM: undefined,
+  NUM_MERCHANT_ID_WORLDPAY: undefined,
+  NUM_MERCHANT_ID_AMEX: undefined,
+  NUM_MERCHANT_ID_BARCLAYCARD: undefined,
+  NUM_MERCHANT_ID_TSYS: undefined,
+  POS_TERMINAL_ID: undefined,
+  // POS_POS_NUMBER: undefined, DEFINED ABOVE
+  NUM_ENVIRONMENT: undefined,
+  NUM_COMPANY_ID_WORLDPAY: undefined,
+  NUM_BEACH: undefined,
+  NUM_AFD: undefined,
+  POS_ENVIRONMENT: undefined,
+  //****************************** AGC CSV ******************************
+  //E_PORTAL_ZONE DEFINED ABOVE
+  // NUM_NUMCOMM DEFINED ABOVE
+  NUM_MERCHANT_ID_IKANO: undefined,
+  NUM_MERCHANT_ID_PPSUK: undefined,
+  NUM_MERCHANT_ID_EPAYUK: undefined,
+  //POS_TERMINAL_ID DEFINED ABOVE
+  //POS_POS_NUMBER DEFINED ABOVE
+  //****************************** GAX CSV ******************************
+  // E_PORTAL_ZONE DEFINED ABOVE
+  // NUM_NUMCOMM DEFINED ABOVE
+  NUM_MERCHANT_ID_SAVEBACK: undefined,
+  //POS_POS_NUMBER DEFINED ABOVE#
+  //****************************** MASSMOVE CSV ******************************
+  // E_PORTAL_ZONE DEFINED ABOVE
+  CURRENCY: undefined,
+  CUSTOMER_FINANCE_EXTERNAL_ID: undefined,
+  CITY_2: undefined,
+  ZIP_CODE_2: undefined,
+  ADDRESS_1_2: undefined,
+  ADDRESS_2_2: undefined,
+};
+
+////////////////////////////////////////////////////////////////////
+// ADM
 function createADM(data) {
-  let returnArr = [];
-  const lines = data.split('\n')
-  lines.forEach(line => {
-    const values = line.split('\t')
-    returnArr.push({
-      cell1: `${values[0]}>>${values[1]}>>${values[2]}`, 
-      cell2: values[25], 
-      cell3: values[25] % 2 === 0 ? 0 : 1
-    })
-  })
-  return returnArr
+  const ADM_DATA = [];
+  const lines = data.split("\n");
+  lines.forEach((line) => {
+    const values = line.split("\t");
+    blankVariableForm.E_PORTAL_ZONE = `${values[0]}>>${values[1]}>>${values[2]}`;
+    blankVariableForm.POS_POS_NUMBER = values[25];
+    blankVariableForm.POS_PCI4 = values[25] % 2 === 0 ? 0 : 1;
+    ADM_DATA.push({
+      cell1: blankVariableForm.E_PORTAL_ZONE,
+      cell2: blankVariableForm.POS_POS_NUMBER,
+      cell3: blankVariableForm.POS_PCI4,
+    });
+  });
+  return ADM_DATA;
 }
 
-function createCPA(data) {
-  let returnArr = [];
-  const lines = data.split('\n')
-  lines.forEach(line => {
-    const values = line.split('\t')
-    returnArr.push({
-      cell1: `${values[0]}>>${values[1]}>>${values[2]}`,
-      cell2: values[28],
-      cell3: values[13],
-      cell4: values[14],
-      cell5: values[16],
-      cell6: values[22],
-      cell7: values[24],
-      cell8: values[25],
-      cell9: values[27],
-      cell10: "NUM_CompanyID_Worldpay",
-      cell11: "NUM_Beach",
-      cell12: "NUM_AFD",
-      cell13: values[27],
-    })
-  })
-  return returnArr
+////////////////////////////////////////////////////////////////////
+// CPA
+async function createCPA(data) {
+  const CPA_DATA = [];
+  const lines = data.split("\n");
+  await lines.forEach((line) => {
+    const values = line.split("\t");
+    const LEVEL_3_SPLIT = values[2].split("_");
+    blankVariableForm.E_PORTAL_ZONE = `${values[0]}>>${values[1]}>>${values[2]}`;
+    blankVariableForm.NUM_NUMCOMM = "SPEAK TO MIKE S TO FIGURE THIS ONE OUT!!!"; // ASK MIKE S ABOUT THIS FUNCTION AND THE FUNCTION FOR NUM_MERCHANT_ID_TSYS!!!
+    blankVariableForm.NUM_MERCHANT_ID_WORLDPAY = values[13];
+    blankVariableForm.NUM_MERCHANT_ID_AMEX = values[14];
+    blankVariableForm.NUM_MERCHANT_ID_BARCLAYCARD = values[16];
+    blankVariableForm.NUM_MERCHANT_ID_TSYS = values[22] === "NULL" ? "4000" + blankVariableForm.NUM_NUMCOMM.split("").slice(2, 5) : values[22]; // prettier-ignore
+    blankVariableForm.POS_TERMINAL_ID = values[24];
+    blankVariableForm.POS_POS_NUMBER = values[25];
+    blankVariableForm.NUM_ENVIRONMENT = LEVEL_3_SPLIT[1];
+    blankVariableForm.NUM_COMPANY_ID_WORLDPAY = "SPEAK TO MIKE S TO FIGURE THIS ONE OUT!!!"; // prettier-ignore
+    blankVariableForm.NUM_BEACH = "SPEAK TO MIKE S TO FIGURE THIS ONE OUT!!!";
+    blankVariableForm.NUM_AFD = "SPEAK TO MIKE S TO FIGURE THIS ONE OUT!!!";
+    blankVariableForm.POS_ENVIRONMENT = LEVEL_3_SPLIT[1];
+
+    CPA_DATA.push({
+      cell1: blankVariableForm.E_PORTAL_ZONE,
+      cell2: blankVariableForm.NUM_NUMCOMM,
+      cell3: blankVariableForm.NUM_MERCHANT_ID_WORLDPAY,
+      cell4: blankVariableForm.NUM_MERCHANT_ID_AMEX,
+      cell5: blankVariableForm.NUM_MERCHANT_ID_BARCLAYCARD,
+      cell6: blankVariableForm.NUM_MERCHANT_ID_TSYS,
+      cell7: blankVariableForm.POS_TERMINAL_ID,
+      cell8: blankVariableForm.POS_POS_NUMBER,
+      cell9: blankVariableForm.NUM_ENVIRONMENT,
+      cell10: blankVariableForm.NUM_COMPANY_ID_WORLDPAY,
+      cell11: blankVariableForm.NUM_BEACH,
+      cell12: blankVariableForm.NUM_AFD,
+      cell13: blankVariableForm.POS_ENVIRONMENT,
+    });
+  });
+  return CPA_DATA;
 }
 
-function createAGC(data) {
-  let returnArr = [];
-  const lines = data.split('\n')
-  lines.forEach(line => {
-    const values = line.split('\t')
-    console.log(values)
-    returnArr.push({
-      cell1: `${values[0]}>>${values[1]}>>${values[2]}`,
-      cell2: values[28], 
-      cell3: "NUM_MerchantID_Ikano",
-      cell4: values[22],
-      cell5: "NUM_MerchantID_PPSUK",
-      // cell6: ,
-      // cell7: ,
-      // cell8: ,
-      // cell9: ,
-      // cell10: ,
-      // cell11: ,
-      // cell12: ,
-      // cell13:
-    })
-  })
-  return returnArr
-}
+////////////////////////////////////////////////////////////////////
+// AGC
+async function createAGC(data) {
+  const AGC_DATA = [];
+  const lines = data.split("\n");
+  await lines.forEach((line) => {
+    const values = line.split("\t");
+    blankVariableForm.E_PORTAL_ZONE = `${values[0]}>>${values[1]}>>${values[2]}`;
+    blankVariableForm.NUM_NUMCOMM = "SPEAK TO MIKE S TO FIGURE THIS ONE OUT!!!"; // ASK MIKE S ABOUT THIS FUNCTION AND THE FUNCTION FOR NUM_MERCHANT_ID_TSYS!!!
+    blankVariableForm.NUM_MERCHANT_ID_IKANO = 79569999;
+    blankVariableForm.NUM_MERCHANT_ID_PPSUK = values[21];
+    blankVariableForm.NUM_MERCHANT_ID_EPAYUK = values[20];
+    blankVariableForm.POS_TERMINAL_ID = values[24];
+    blankVariableForm.POS_POS_NUMBER = values[25];
 
-// function createGAX(data) {
-//   let returnArr = [];
-//   const lines = data.split('\n')
-//   lines.forEach(line => {
-//     const values = line.split('\t')
-//     returnArr.push({
-//       cell1: ,
-//       cell2: , 
-//       cell3: ,
-//       cell4: ,
-//       cell5: ,
-//       cell6: ,
-//       cell7: ,
-//       cell8: ,
-//       cell9: ,
-//       cell10: ,
-//       cell11: ,
-//       cell12: ,
-//       cell13:
-//     })
-//   })
-//   return returnArr
-// }
+    AGC_DATA.push({
+      cell1: blankVariableForm.E_PORTAL_ZONE,
+      cell2: blankVariableForm.NUM_NUMCOMM,
+      cell3: blankVariableForm.NUM_MERCHANT_ID_IKANO,
+      cell4: blankVariableForm.NUM_MERCHANT_ID_PPSUK,
+      cell5: blankVariableForm.NUM_MERCHANT_ID_EPAYUK,
+      cell6: blankVariableForm.POS_TERMINAL_ID,
+      cell7: blankVariableForm.POS_POS_NUMBER,
+    });
+  });
+  return AGC_DATA;
+}
+////////////////////////////////////////////////////////////////////
+// GAX
+async function createGAX(data) {
+  const GAX_DATA = [];
+  const lines = data.split("\n");
+  await lines.forEach((line) => {
+    const values = line.split("\t");
+    blankVariableForm.E_PORTAL_ZONE = `${values[0]}>>${values[1]}>>${values[2]}`;
+    blankVariableForm.NUM_NUMCOMM = "SPEAK TO MIKE S TO FIGURE THIS ONE OUT!!!"; // ASK MIKE S ABOUT THIS FUNCTION AND THE FUNCTION FOR NUM_MERCHANT_ID_TSYS!!!
+    blankVariableForm.NUM_MERCHANT_ID_SAVEBACK = "00000000000";
+    blankVariableForm.POS_POS_NUMBER = values[25];
+
+    GAX_DATA.push({
+      cell1: blankVariableForm.E_PORTAL_ZONE,
+      cell2: blankVariableForm.NUM_NUMCOMM,
+      cell3: blankVariableForm.NUM_MERCHANT_ID_SAVEBACK,
+      cell4: blankVariableForm.POS_POS_NUMBER,
+    });
+  });
+  return GAX_DATA;
+}
 
 module.exports = {
-    createADM,
-    createCPA,
-    createAGC,
-    // createGAX
-}
+  createADM,
+  createCPA,
+  createAGC,
+  createGAX,
+};
