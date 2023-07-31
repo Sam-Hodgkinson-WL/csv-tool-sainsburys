f3cad750438b1ad89690949616bd34d13ce177ba
Hodgkinson <sam.hodgkinson@worldline.com>
2023-07-31 12:16:32 +0100

    Created the open.sh file for easily launching the service and added some instructions to the README file

---
+++ tool/README.md
@@ -0,0 +1,25 @@
+# How to install and run this on your machine
+
+1. Go to [GitHub - Sam-Hodgkinson-WL/csv-tool-sainsburys](https://github.com/Sam-Hodgkinson-WL/csv-tool-sainsburys)
+
+2. Download the .zip file and extract it's contents to wherever you want to install the tool, I have mine in C:/Program_Files
+
+3. Copy and paste the open.sh somewhere accessible, I have mine on my Desktop
+
+4. Double click on the open.sh file (you might have to install git bash first) https://git-scm.com/download/win
+
+5. There will be some messages that appear in the terminal window, you won't have to do anything, but wait for the message to say you can close the terminal
+
+6. The app will now be running on port 3000, you can access it by going to your browser and going to http://localhost:3000
+
+7. This service will run until you shutdown or restart your machine.
+
+## Alternatively, if something goes wrong with the shell script,
+
+1. Open git bash (or the terminal of your choice and cd to where the tool is installed)
+
+2. You want to be able to see the package.json file when either the ls command (git) or dir command (powershell/cmd) is run
+
+3. Run the command 'npm i'
+
+4. Once complete, run 'npm start'
---
+++ tool/open.sh
@@ -0,0 +1,38 @@
+# Function to start npm start in the background and store the process ID in a file
+start_npm() {
+  npm start &
+  echo $! > npm_process.pid
+}
+
+# Function to stop the npm start process when the script is terminated
+stop_npm() {
+  if [ -f npm_process.pid ]; then
+    npm_process_id=$(cat npm_process.pid)
+    rm npm_process.pid
+    kill $npm_process_id
+  fi
+}
+
+# Function to handle SIGHUP signal (terminal closure)
+handle_sighup() {
+  stop_npm
+  exit 0
+}
+
+# Navigate to the directory
+cd "C:\\Program_Files\\sains-tool\\tool"  # Use double backslashes here and change the path to where you have the app downloaded to
+
+# Run npm install without printing errors to console
+echo "This could take a minute, please don't close this window"
+npm i > /dev/null 2>&1
+
+# Start npm start in the background
+start_npm
+
+# Trap the termination signals to stop npm start gracefully
+trap handle_sighup EXIT SIGINT SIGTERM SIGHUP
+
+# Wait until the script is terminated (you can add more commands here if needed)
+echo 'You can now close this window, if you close the browser tab with the tool you will be able to get back to it by going to http://localhost:3000 again'
+echo 'You only need to run this once when you start your PC up, the service will be active until you shutdown or restart'
+wait
